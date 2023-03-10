7cb69e81c3b181e37a03cd9c5bd6ec47a7ac9de5
Maciej Gierada <maciek.gierada@gmail.com>
2023-03-10 09:47:04 +0100

    feat: load env variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use dotenv::dotenv;
 fn main() {
+    dotenv().ok();
+    let goove_api_token = std::env::var("GOVEE_API_KEY").expect("GOVEE_API_KEY must be set.");
     println!("Hello, world!");
+    println!("GOVEE_API_KEY: {}", goove_api_token);
 }
