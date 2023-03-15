db9c26a4f54ed2d596d724ea581f0cbd63a88999
Maciej Gierada <maciek.gierada@gmail.com>
2023-03-15 16:46:20 +0100

    feat: read env file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate rocket;
-use dotenv::dotenv;
 
+use dotenv::dotenv;
 use lazy_static::lazy_static;
 use routes::healthcheck::healthcheck_handler;
 use routes::office_lamp_routes::{office_off_handler, office_on_handler};
@@ -11,37 +11,19 @@ use std::env::var;
 pub mod routes;
 pub mod services;
 
-// const GOVEE_API_KEY: &str = match std::env::var("API_KEY") {
-//     Ok(val) => val.as_str(),
-//     Err(_) => panic!("API_KEY environment variable not set"),
-// };
-//
-//
-// set GOVEE_API_KEY as a const read from env variables
-// const GOVEE_API_KEY: &String = &std::env::var("GOVEE_API_KEY").expect("GOVEE_API_KEY must be set.");
-// static GOVEE_API_KEY: &str = &std::env::var("GOVEE_API_KEY").expect("GOVEE_API_KEY must be set.");
-// static GOVEE_API_KEY: String = std::env::var("GOVEE_API_KEY")
-//     .expect("GOVEE_API_KEY must be set.")
-//     .unwrap();
-//
-
 lazy_static! {
     pub static ref GOVEE_API_KEY: String =
         var("GOVEE_API_KEY").expect("GOVEE_API_KEY must be set.");
 }
-
-fn get_govee_api_key() -> String {
-    dotenv().ok();
-    std::env::var("GOVEE_API_KEY").expect("GOVEE_API_KEY must be set.")
-}
-
-fn get_goove_root_url() -> String {
-    dotenv().ok();
-    std::env::var("GOVEE_ROOT_URL").expect("GOVEE_ROOT_URL must be set.")
+lazy_static! {
+    pub static ref GOVEE_ROOT_URL: String =
+        var("GOVEE_ROOT_URL").expect("GOVEE_ROOT_URL must be set.");
 }
 
 #[launch]
 fn rocket() -> _ {
+    // read .env file
+    dotenv().ok();
     rocket::build()
         .mount("/tv", routes![tv_on_handler, tv_off_handler])
         .mount("/office", routes![office_on_handler, office_off_handler])
--- src/routes/office_lamp_routes.rs
+++ src/routes/office_lamp_routes.rs
@@ -2,20 +2,18 @@ use rocket::serde::json::Json;
 
 use crate::services::govee_api_service::sent_put_request;
 use crate::services::light_setup_service::office_light_setup;
-use crate::{get_goove_root_url, GOVEE_API_KEY};
+use crate::{GOVEE_API_KEY, GOVEE_ROOT_URL};
 
 #[get("/on")]
 pub async fn office_on_handler() -> Json<serde_json::Value> {
-    let govee_root_url = get_goove_root_url();
     let payload = office_light_setup("on");
-    sent_put_request(&govee_root_url, &GOVEE_API_KEY, payload).await;
+    sent_put_request(&GOVEE_ROOT_URL, &GOVEE_API_KEY, payload).await;
     Json(serde_json::json!({"device": "office_light", "status": "on"}))
 }
 
 #[get("/off")]
 pub async fn office_off_handler() -> Json<serde_json::Value> {
-    let govee_root_url = get_goove_root_url();
     let payload = office_light_setup("off");
-    sent_put_request(&govee_root_url, &GOVEE_API_KEY, payload).await;
+    sent_put_request(&GOVEE_ROOT_URL, &GOVEE_API_KEY, payload).await;
     Json(serde_json::json!({"device": "office_light", "status": "off"}))
 }
