a91992d368d34216c0bf7c9fb82fed1134cc1c39
Maciej Gierada <maciek.gierada@gmail.com>
2023-03-15 09:58:09 +0100

    feat: use GOVEE root url

--- src/routes/office_lamp_routes.rs
+++ src/routes/office_lamp_routes.rs
@@ -1,22 +1,22 @@
 use rocket::serde::json::Json;
 
 use crate::services::govee_api_service::sent_put_request;
-use crate::{get_govee_api_key, office_light_setup};
+use crate::{get_govee_api_key, office_light_setup, get_goove_root_url};
 
 #[get("/on")]
 pub async fn office_on_handler() -> Json<serde_json::Value> {
     let govee_api_key = get_govee_api_key();
-    let govee_api_url = "https://developer-api.govee.com/v1/devices/control";
+    let govee_root_url = get_goove_root_url();
     let payload = office_light_setup("on");
-    sent_put_request(govee_api_url, &govee_api_key, payload).await;
+    sent_put_request(&govee_root_url, &govee_api_key, payload).await;
     Json(serde_json::json!({"device": "office_light", "status": "on"}))
 }
 
 #[get("/off")]
 pub async fn office_off_handler() -> Json<serde_json::Value> {
     let govee_api_key = get_govee_api_key();
-    let govee_api_url = "https://developer-api.govee.com/v1/devices/control";
+    let govee_root_url = get_goove_root_url();
     let payload = office_light_setup("off");
-    sent_put_request(govee_api_url, &govee_api_key, payload).await;
+    sent_put_request(&govee_root_url, &govee_api_key, payload).await;
     Json(serde_json::json!({"device": "office_light", "status": "off"}))
 }
