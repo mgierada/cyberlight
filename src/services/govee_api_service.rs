ebfff812748dd3e76436071ee74285497704c081
Maciej Gierada <maciek.gierada@gmail.com>
2023-03-16 09:46:06 +0100

    feat: add get request to get all devices

--- src/services/govee_api_service.rs
+++ src/services/govee_api_service.rs
@@ -1,29 +1,72 @@
 use reqwest::Client;
+use serde::Deserialize;
 use serde_json::json;
 
 use super::light_setup_service::PayloadBody;
 
-#[derive(Debug)]
-struct ApiResponse {
+#[derive(Debug, Deserialize)]
+pub struct ApiResponseAllDevices {
     code: i16,
     message: String,
     data: Option<Data>,
 }
-#[derive(Debug)]
-struct Data {}
+#[derive(Debug, Deserialize)]
+pub struct Data {
+    devices: Vec<Device>,
+}
+#[derive(Debug, Deserialize)]
+pub struct Device {
+    device: String,
+    model: String,
+    deviceName: String,
+    controllable: bool,
+    retrievable: bool,
+    supportCmds: Vec<String>,
+    properties: Properties,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Properties {
+    colorTem: ColorTem,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ColorTem {
+    range: ColorTemRange,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ColorTemRange {
+    min: i16,
+    max: i16,
+}
 
 pub async fn sent_put_request(
-    govee_api_url: &str,
+    govee_root_url: &str,
     govee_api_key: &str,
     payload: PayloadBody,
 ) -> () {
     let client = Client::new();
     let payload_json = json!(payload);
+    let endpoint = format!("{}/v1/devices/control", govee_root_url);
     let _response = client
-        .put(govee_api_url)
+        .put(endpoint)
         .header("Govee-API-Key", govee_api_key)
         .json(&payload_json)
         .send()
         .await
         .unwrap();
+}
+
+pub async fn get_all_devices(govee_root_url: &str, govee_api_key: &str) -> ApiResponseAllDevices {
+    let client = Client::new();
+    let endpoint = format!("{}/v1/devices", govee_root_url);
+    let response = client
+        .get(endpoint)
+        .header("Govee-API-Key", govee_api_key)
+        .send()
+        .await
+        .unwrap();
+    let response_json: ApiResponseAllDevices = response.json().await.unwrap();
+    response_json
 }
