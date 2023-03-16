a364ace846d9026674f06454dcdef2e7f183d285
Maciej Gierada <maciek.gierada@gmail.com>
2023-03-16 10:31:30 +0100

    feat: response from external api wrapped

--- src/routes/get_all_devices_routes.rs
+++ src/routes/get_all_devices_routes.rs
@@ -1,11 +1,10 @@
-
 use rocket::serde::json::Json;
 
 use crate::services::govee_api_service::{get_all_devices, ApiResponseAllDevices};
 use crate::{GOVEE_API_KEY, GOVEE_ROOT_URL};
 
 #[get("/devices")]
-pub async fn get_all_devices_handler() -> Json<ApiResponseAllDevices>{
-    let all_devices= get_all_devices(&GOVEE_ROOT_URL, &GOVEE_API_KEY).await;
-    Json(all_devices)
+pub async fn get_all_devices_handler() -> Json<serde_json::Value> {
+    let all_devices: ApiResponseAllDevices = get_all_devices(&GOVEE_ROOT_URL, &GOVEE_API_KEY).await;
+    Json(serde_json::json!({ "devices": all_devices }))
 }
