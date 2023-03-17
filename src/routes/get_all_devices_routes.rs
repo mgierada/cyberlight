ec7bec810ec2797872dabe1d4ee5fa758290a027
Maciej Gierada <maciek.gierada@gmail.com>
2023-03-17 09:24:23 +0100

    feat: map works well

--- src/routes/get_all_devices_routes.rs
+++ src/routes/get_all_devices_routes.rs
@@ -1,17 +1,34 @@
 use rocket::serde::json::Json;
+use serde::Serialize;
 
 use crate::services::govee_api_service::{get_all_devices, ApiResponseAllDevices};
 use crate::{GOVEE_API_KEY, GOVEE_ROOT_URL};
 
+#[derive(Debug, Serialize)]
+#[allow(non_snake_case)]
+struct GoveeDevice {
+    deviceName: String,
+    device: String,
+    model: String,
+    controllable: bool,
+    retrievable: bool,
+}
+
 #[get("/devices")]
 pub async fn get_all_devices_handler() -> Json<serde_json::Value> {
-    let all_devices_response: ApiResponseAllDevices = get_all_devices(&GOVEE_ROOT_URL, &GOVEE_API_KEY).await;
+    let all_devices_response: ApiResponseAllDevices =
+        get_all_devices(&GOVEE_ROOT_URL, &GOVEE_API_KEY).await;
     let all_devices = all_devices_response.data.unwrap().devices;
-    let trim_response = all_devices.iter().map(|device| device.deviceName.clone()).collect::<Vec<String>>();
+    let trim_response = all_devices
+        .iter()
+        .map(|device| GoveeDevice {
+            deviceName: device.deviceName.clone(),
+            device: device.device.clone(),
+            model: device.model.clone(),
+            controllable: device.controllable,
+            retrievable: device.retrievable,
+        })
+        .collect::<Vec<GoveeDevice>>();
 
-
-
-     
-
-    Json(serde_json::json!({ "devices": trim_response}))
+    Json(serde_json::json!({ "devices": trim_response }))
 }
