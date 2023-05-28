e6e3ab5de2786d67832ba5385163b047e235bcb2
Maciej Gierada <maciek.gierada@gmail.com>
2023-05-28 22:30:28 +0200

    feat: add more commands to set up the server

--- utils/server_setup.sh
+++ utils/server_setup.sh
@@ -9,3 +9,12 @@ sudo service docker start
 sudo usermod -a -G docker ubuntu
 # Source the .bashrc file to apply changes or logout and login again
 source ~/.bashrc
+# Install AWS CLI
+sudo apt install awscli
+# Configure aws cli
+aws configure
+# Login to ECR
+$(aws ecr get-login --no-include-email --region us-east-2)
+# pull docker image from ECR
+docker pull 458814646445.dkr.ecr.eu-north-1.amazonaws.com/rust_that_light:latest
+
