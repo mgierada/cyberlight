fffece0f573085c2933c88f3102f450124578312
Maciej Gierada <maciek.gierada@gmail.com>
2023-06-03 23:19:11 +0200

    feat: almost there - need to mock respons from all devices

--- src/tests/test_services/test_govee_api_service.rs
+++ src/tests/test_services/test_govee_api_service.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::services::{
-        govee_api_service::sent_put_request,
+        govee_api_service::{get_all_devices, sent_put_request},
         light_setup_service::{GoveeCommand, PayloadBody},
     };
     use mockito;
@@ -25,7 +25,21 @@ mod tests {
             model: "model_id".to_string(),
             cmd: command,
         };
-        sent_put_request(&govee_root_url, govee_api_key, payload).await;
+        sent_put_request(&govee_root_url, &govee_api_key, payload).await;
+        mock_endpoint.assert();
+    }
+
+    #[tokio::test]
+    async fn test_get_all_devices() {
+        let mut server = mockito::Server::new();
+        let govee_root_url = server.url();
+        let govee_api_key = "1234567890";
+        let mock_endpoint = server
+            .mock("get", "/v1/devices")
+            .match_header("govee-api-key", govee_api_key)
+            .with_status(200)
+            .create();
+        get_all_devices(&govee_root_url, &govee_api_key).await;
         mock_endpoint.assert();
     }
 }
