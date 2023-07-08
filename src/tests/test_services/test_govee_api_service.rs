a999059e168d89ad6dda52b5d31ef5d97db2321e
Maciej Gierada <maciek.gierada@gmail.com>
2023-07-08 22:02:44 +0200

    feat: add unit test for test_get_device_status

--- src/tests/test_services/test_govee_api_service.rs
+++ src/tests/test_services/test_govee_api_service.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::services::{
-        govee_api_service::{get_all_devices, sent_put_request},
+        govee_api_service::{get_all_devices, get_device_status, sent_put_request},
         light_setup_service::{GoveeCommand, PayloadBody},
     };
     use mockito;
@@ -69,6 +69,46 @@ mod tests {
             )
             .create();
         get_all_devices(&govee_root_url, &govee_api_key).await;
+        mock_endpoint.assert();
+    }
+
+    #[tokio::test]
+    async fn test_get_device_status() {
+        let mut server = mockito::Server::new();
+        let gooee_root_url = server.url();
+        let govee_api_key = "1234567890";
+        let device = "device_name";
+        let model = "model_name";
+        let mock_endpoint = server
+            .mock("get", "/v1/devices/state")
+            .match_header("govee-api-key", govee_api_key)
+            .match_query(mockito::Matcher::AllOf(vec![
+                mockito::Matcher::UrlEncoded("device".into(), device.into()),
+                mockito::Matcher::UrlEncoded("model".into(), model.into()),
+            ]))
+            .with_status(200)
+            .with_body(
+                r#"{
+                    "code": 200,
+                    "message": "Success",
+                    "device": {
+                        "device": "device_id",
+                        "model": "model_id",
+                        "properties": {
+                            "color": {
+                                "r": 255,
+                                "g": 255,
+                                "b": 255
+                            },
+                            "brightness": 100,
+                            "powerState": "on",
+                            "colorTem": 9001
+                        }
+                    }
+                }"#,
+            )
+            .create();
+        get_device_status(&gooee_root_url, &govee_api_key, device, model).await;
         mock_endpoint.assert();
     }
 }
