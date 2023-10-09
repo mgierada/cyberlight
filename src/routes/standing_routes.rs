332c0fe15e26d6396ebfebfbc59625ae692c9985
Maciej Gierada <maciek.gierada@gmail.com>
2023-10-09 16:41:34 +0200

    feat: redo routes for standing light

---
+++ src/routes/standing_routes.rs
@@ -0,0 +1,54 @@
+use crate::constants::enums::OfficeDevices;
+use crate::implementations::access_token::Token;
+use crate::services::light_setup_service::office_light_setup;
+use crate::GOVEE_API_KEY;
+use govee_api::GoveeClient;
+use rocket::serde::json::Json;
+
+#[get("/standing/right/on")]
+pub async fn standing_right_on_handler(_token: Token) -> Json<serde_json::Value> {
+    let standing_right_led = OfficeDevices::standing_right_led();
+    let payload = office_light_setup(&standing_right_led, "on");
+    let govee_client = GoveeClient::new(&GOVEE_API_KEY);
+    let result = govee_client.control_device(payload).await;
+    if let Err(err) = result {
+        panic!("Error occurred: {:?}", err);
+    }
+    Json(serde_json::json!({"device": "standing_right_led", "status": "on"}))
+}
+
+#[get("/standing/right/off")]
+pub async fn standing_right_off_handler(_token: Token) -> Json<serde_json::Value> {
+    let standing_right_led = OfficeDevices::standing_right_led();
+    let payload = office_light_setup(&standing_right_led, "off");
+    let govee_client = GoveeClient::new(&GOVEE_API_KEY);
+    let result = govee_client.control_device(payload).await;
+    if let Err(err) = result {
+        panic!("Error occurred: {:?}", err);
+    }
+    Json(serde_json::json!({"device": "standing_right_led", "status": "off"}))
+}
+
+#[get("/standing/left/on")]
+pub async fn standing_left_on_handler(_token: Token) -> Json<serde_json::Value> {
+    let standing_left_led = OfficeDevices::standing_left_led();
+    let payload = office_light_setup(&standing_left_led, "on");
+    let govee_client = GoveeClient::new(&GOVEE_API_KEY);
+    let result = govee_client.control_device(payload).await;
+    if let Err(err) = result {
+        panic!("Error occurred: {:?}", err);
+    }
+    Json(serde_json::json!({"device": "standing_left_led", "status": "on"}))
+}
+
+#[get("/standing/left/off")]
+pub async fn standing_left_off_handler(_token: Token) -> Json<serde_json::Value> {
+    let standing_left_led = OfficeDevices::standing_left_led();
+    let payload = office_light_setup(&standing_left_led, "off");
+    let govee_client = GoveeClient::new(&GOVEE_API_KEY);
+    let result = govee_client.control_device(payload).await;
+    if let Err(err) = result {
+        panic!("Error occurred: {:?}", err);
+    }
+    Json(serde_json::json!({"device": "standing_left_led", "status": "off"}))
+}
